8fd295ee9b1f49de904f124d8740906a9b22a7f5
Andy <andypicke@gmail.com>
2024-05-30 15:54:22 -0600

    add default function param

--- R/construct_query_url.R
+++ R/construct_query_url.R
@@ -1,9 +1,9 @@
 #' Build url query
-#' @param wh_state State
+#' @param wh_state State to return data for (default is "all")
 #' @returns request_url: query url
 #' @export
 
-construct_query_url <- function(wh_state){
+construct_query_url <- function(wh_state = "all"){
 
   base_url <- "https://eersc.usgs.gov/api/uspvdb/v1/"
 
@@ -16,12 +16,13 @@ construct_query_url <- function(wh_state){
   # return only specific columns:
   #"https://eersc.usgs.gov/api/uspvdb/v1/projects?&select=p_name,p_cap_ac,p_state&order=p_name.desc"
 
-#  wh_state <- "CO"
 
-  #param_str <- "?&case_id=eq.400004"
-  param_str <- paste0("?&p_state=eq.", wh_state)
-  #param_str <- ""
-  #request_url <- paste0(base_url, endpoint)
+  if (wh_state == "all") {
+    param_str <- ""
+  } else {
+    param_str <- paste0("?&p_state=eq.", wh_state)
+  }
+
   request_url <- paste0(base_url, endpoint, param_str)
 
 }
--- R/get_uspvdb_data.R
+++ R/get_uspvdb_data.R
@@ -1,9 +1,9 @@
 #' Get USPVDB data
-#' @param wh_state State to get data for
+#' @param wh_state State to get data for (default is "all")
 #' @returns df Dataframe of requested data from USPVDB API
 #' @export
 
-get_uspvdb_data <- function(wh_state){
+get_uspvdb_data <- function(wh_state = "all"){
 
   req_url <- construct_query_url(wh_state = wh_state)
 
